bc5b355971af4920ba3cc5b0b2d02ce5367ba84f
RAIMONES <raimonesrg3@gmail.com>
2023-01-27 19:14:11 -0300

    main

--- js/main.js
+++ js/main.js
@@ -1,19 +1,39 @@
+import { PersonView } from "./mvc/view/PersonView.js"
 import { PersonController } from "./mvc/controller/PersonController.js"
 
+// Instanciar modelo de visualização
+let personView       = new PersonView()
+// Instanciar modelo de controlador
 let personController = new PersonController()
 
-const form           = document.querySelector('form')
-const msg            = document.querySelector('.msg')
+// Iniciar visualização da tabela
+personView.initTable()
+
+// Mostrar dados guardados
+if(localStorage.getItem('_repository')){
+  personController.show()
+} else {
+// Criar repositório para guardar dados
+  localStorage.setItem('_repository', [])
+}
+
+// Informações de formulário
+const form = document.querySelector('form')
 
 form.addEventListener('submit', evt=>{
-  
-  msg.classList.remove('disable')
-  
+  // Enviar o evento ao controlador
   personController._add(evt)
-  
+  // Pedir para o controlador limpar o formulário
   personController._formClean()
   
-  setTimeout(()=>{
-    msg.classList.add('disable')
-  }, 1500)
 })
+
+// Apaga linha da tabela parte 2/3
+export function toClearElement(element){
+  personController.clearElement(element)
+}
+
+// Apaga toda tabela parte 2/3
+export function toClearTable(){
+  personController.clearTable()
+}
